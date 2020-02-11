87db6c7434112f617929d16e547012981b5c3cf5
Marcos Lopes <marcos@dourasoft.com.br>
2020-02-11 12:40:41 -0400

    Correções no  layout e funcionalidades parte 1

--- config/server.js
+++ config/server.js
@@ -9,8 +9,8 @@ const session = require('express-session');
 // 	console.log("rodando na porta 3000.");
 // });
 
-app.listen(process.env.PORT_APP, () => {
-	console.log('Servidor inicializado na porta:' + process.env.PORT_APP);
+app.listen(process.env.PORT, () => {
+	console.log('Servidor inicializado na porta:' + process.env.PORT);
 });
 
 app.set('views', './app/views');
