b95252a9710872d6c3566c57d6cacdad2c49aaea
Marcos Lopes <marcos@dourasoft.com.br>
2020-02-11 12:44:41 -0400

    Correções no  layout e funcionalidades parte 1

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,18 @@
 module.exports = function (app) {
 	console.log(app)
 	app.get('/', function (req, res) {
-		app.app.controllers.index.index(app, req, res);
+		app.app.index.index(app, req, res);
 	})
 	app.get('/dados/pessoais', function (req, res) {
-		app.app.controllers.index.dados_pessoais(app, req, res);
+		app.app.index.dados_pessoais(app, req, res);
 	});
 	app.get('/esquece/senha', function (req, res) {
-		app.app.controllers.index.esqueceu_senha(app, req, res);
+		app.app.index.esqueceu_senha(app, req, res);
 	})
 	app.post('/alterar/dados/pessoais', function (req, res) {
-		app.app.controllers.index.update_dados_pessoais(app, req, res);
+		app.app.index.update_dados_pessoais(app, req, res);
 	});
 	app.post('/senha', function (req, res) {
-		app.app.controllers.index.senha_mudar(app, req, res);
+		app.app.index.senha_mudar(app, req, res);
 	})
 }
