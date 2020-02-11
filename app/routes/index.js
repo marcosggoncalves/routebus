d250ffb1bca46f8186a3a193e82279574260d2bb
Marcos Lopes <marcos@dourasoft.com.br>
2020-02-11 12:49:54 -0400

    asd

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,23 @@
 module.exports = function (app) {
 	console.log("--------------------------------------------")
 	console.log(app.controllers)
-	console.log(app.routes)
+	console.log(app.controllers.index)
+	console.log(app.controllers.index.index)
 	console.log(app.models)
 	console.log("--------------------------------------------")
 	app.get('/', function (req, res) {
-		app.app.index.index(app, req, res);
+		app.app.controllers.index.index(app, req, res);
 	})
 	app.get('/dados/pessoais', function (req, res) {
-		app.app.index.dados_pessoais(app, req, res);
+		app.app.controllers.index.dados_pessoais(app, req, res);
 	});
 	app.get('/esquece/senha', function (req, res) {
-		app.app.index.esqueceu_senha(app, req, res);
+		app.app.controllers.index.esqueceu_senha(app, req, res);
 	})
 	app.post('/alterar/dados/pessoais', function (req, res) {
-		app.app.index.update_dados_pessoais(app, req, res);
+		app.app.controllers.index.update_dados_pessoais(app, req, res);
 	});
 	app.post('/senha', function (req, res) {
-		app.app.index.senha_mudar(app, req, res);
+		app.app.controllers.index.senha_mudar(app, req, res);
 	})
 }
