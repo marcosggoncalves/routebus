17620b09f2adf84560bec37cd262d13def2f8862
Marcos Lopes <marcos@dourasoft.com.br>
2020-02-11 12:51:54 -0400

    marcos é homem

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,18 +6,18 @@ module.exports = function (app) {
 	console.log(app.models)
 	console.log("--------------------------------------------")
 	app.get('/', function (req, res) {
-		app.app.controllers.index.index(app, req, res);
+		app.controllers.index.index(app, req, res);
 	})
 	app.get('/dados/pessoais', function (req, res) {
-		app.app.controllers.index.dados_pessoais(app, req, res);
+		app.controllers.index.dados_pessoais(app, req, res);
 	});
 	app.get('/esquece/senha', function (req, res) {
-		app.app.controllers.index.esqueceu_senha(app, req, res);
+		app.controllers.index.esqueceu_senha(app, req, res);
 	})
 	app.post('/alterar/dados/pessoais', function (req, res) {
-		app.app.controllers.index.update_dados_pessoais(app, req, res);
+		app.controllers.index.update_dados_pessoais(app, req, res);
 	});
 	app.post('/senha', function (req, res) {
-		app.app.controllers.index.senha_mudar(app, req, res);
+		app.controllers.index.senha_mudar(app, req, res);
 	})
 }
